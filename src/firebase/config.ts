cdb17acafed1f49c340def387687b3692c75c2d0
Lu <117615336+lu-developer476@users.noreply.github.com>
2026-03-03 03:57:06 -0300

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app'
-import { getAuth } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
+import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -9,16 +7,15 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD6C6ct-JequfvNkEAi2cIqmcgFvQTPD3c",
+  apiKey: "AIzaSyBPaZWc2lnb2FB9riXXG5cY_HFfLkjp73U",
   authDomain: "art-syntex.firebaseapp.com",
   projectId: "art-syntex",
   storageBucket: "art-syntex.firebasestorage.app",
-  messagingSenderId: "123384151241",
-  appId: "1:123384151241:web:f833ca39fa256503466118",
-  measurementId: "G-M7SZW2DLEH"
+  messagingSenderId: "928663003142",
+  appId: "1:928663003142:web:b827043034ff1fbd66e387",
+  measurementId: "G-L8811N2Z12"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-export const db = getFirestore(app)
