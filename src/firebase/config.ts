22fc74dac7a63697283ff0f7598280cfd836bd0f
Lu <117615336+lu-developer476@users.noreply.github.com>
2026-03-03 00:55:36 -0300

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,13 +3,14 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-}
+  apiKey: "AIzaSyD6C6ct-JequfvNkEAi2cIqmcgFvQTPD3c",
+  authDomain: "art-synt-13037.firebaseapp.com",
+  projectId: "art-synt-13037",
+  storageBucket: "art-synt-13037.firebasestorage.app",
+  messagingSenderId: "123384151241",
+  appId: "1:123384151241:web:f833ca39fa256503466118",
+  measurementId: "G-M7SZW2DLEH"
+};
 
 export const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
