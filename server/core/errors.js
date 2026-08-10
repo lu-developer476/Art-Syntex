2ae0298523c368f204448808977d7fd19b6a7f21
Lu <117615336+lu-developer476@users.noreply.github.com>
2026-08-10 16:59:43 -0300

    refactor: add backend application errors

---
+++ server/core/errors.js
@@ -0,0 +1,22 @@
+export class AppError extends Error {
+  constructor(message, statusCode = 500, details = undefined) {
+    super(message)
+    this.name = 'AppError'
+    this.statusCode = statusCode
+    this.details = details
+  }
+}
+
+export class ValidationError extends AppError {
+  constructor(message, details) {
+    super(message, 400, details)
+    this.name = 'ValidationError'
+  }
+}
+
+export class ExternalServiceError extends AppError {
+  constructor(message = 'Unable to process the request right now.') {
+    super(message, 502)
+    this.name = 'ExternalServiceError'
+  }
+}
